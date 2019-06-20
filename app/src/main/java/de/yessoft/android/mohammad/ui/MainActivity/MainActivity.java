fdea46906b4eee5c871c864a4dc91cceadb4bc8e
MickSawy3r <Mohammad.al.kalaleeb@gmail.com>
2019-06-20 14:32:28 +0300

    Color Correction, Changed it to Blue, and Contact us Added

--- app/src/main/java/de/yessoft/android/mohammad/ui/MainActivity/MainActivity.java
+++ app/src/main/java/de/yessoft/android/mohammad/ui/MainActivity/MainActivity.java
@@ -1,6 +1,10 @@
 package de.yessoft.android.mohammad.ui.MainActivity;
 
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.View;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
@@ -45,19 +49,22 @@ public class MainActivity extends AppCompatActivity {
 
         // region Check Login Status and Redirect If Not Logged In
         FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
-
+        if (user != null){
+            Menu menu= mainNavigation.getMenu();
+            menu.getItem(2).setTitle(getString(R.string.sign_out));
+        }
         // endregion
 
         inflateFeed();
 
         // region UI Actions
 
-        mainToolbar.setNavigationOnClickListener ((v) -> {
+        mainToolbar.setNavigationOnClickListener((v) -> {
             drawerLayout.openDrawer(GravityCompat.START);
         });
 
         mainNavigation.setNavigationItemSelectedListener(menuItem -> {
-            switch (menuItem.getItemId()){
+            switch (menuItem.getItemId()) {
                 case R.id.nav_news_feed:
                     inflateFeed();
                     break;
@@ -81,8 +88,43 @@ public class MainActivity extends AppCompatActivity {
 
     // endregion
 
+    public void setupSocialOnClickers(View activityView) {
+
+        switch (activityView.getId()) {
+            case R.id.btn_whatsapp:
+                Intent whatsapp = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.link_chat_whatsapp)));
+                startActivity(whatsapp);
+                break;
+            case R.id.btn_telegram:
+                try {
+                    Intent telegram = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.link_telegram)));
+                    telegram.setPackage("org.telegram.messenger");
+                    startActivity(telegram);
+                } catch (Exception e) {
+                    Toast.makeText(this, "Telegram app is not installed", Toast.LENGTH_LONG).show();
+                }
+                break;
+            case R.id.btn_facebook:
+                Intent facebook = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.link_facebook_page)));
+                startActivity(facebook);
+                break;
+            case R.id.btn_linked_in:
+                Intent linkedin = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.link_linked_in)));
+                startActivity(linkedin);
+                break;
+            case R.id.btn_web:
+                Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(getString(R.string.link_web)));
+                startActivity(intent);
+                break;
+            default:
+                Toast.makeText(this, "Unknow Button Click!", Toast.LENGTH_SHORT).show();
+                break;
+        }
+    }
+
+
     // region Fragment Inflation
-    private void inflateFeed(){
+    private void inflateFeed() {
         // region Load NewsFeedFragment
 
         drawerLayout.closeDrawers();
@@ -98,7 +140,7 @@ public class MainActivity extends AppCompatActivity {
         // endregion
     }
 
-    private void inflateServices(){
+    private void inflateServices() {
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.fl_container, new ServicesFragment())
@@ -124,14 +166,14 @@ public class MainActivity extends AppCompatActivity {
                 RC_SIGN_IN);
     }
 
-    private void handleSignInResult(int resultCode){
+    private void handleSignInResult(int resultCode) {
         if (resultCode == RESULT_OK) {
             // Successfully signed in
             Toast.makeText(this, "Login Success", Toast.LENGTH_SHORT).show();
             FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
             if (user != null) {
                 String userPhone = user.getPhoneNumber();
-                Toast.makeText(this, userPhone , Toast.LENGTH_SHORT).show();
+                Toast.makeText(this, userPhone, Toast.LENGTH_SHORT).show();
             }
 
         } else {
