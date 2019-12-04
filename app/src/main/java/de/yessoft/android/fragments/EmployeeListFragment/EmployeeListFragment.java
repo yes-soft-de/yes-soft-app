d685c2a6500d4da5144dc9121bb05b3c13757f0f
MickSawy3r <Mohammad.al.kalaleeb@gmail.com>
2019-12-04 23:07:25 +0200

    added test cases

--- app/src/main/java/de/yessoft/android/fragments/EmployeeListFragment/EmployeeListFragment.java
+++ app/src/main/java/de/yessoft/android/fragments/EmployeeListFragment/EmployeeListFragment.java
@@ -5,6 +5,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextSwitcher;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
@@ -28,7 +29,10 @@ import de.yessoft.android.entity.Blog;
 import de.yessoft.android.entity.EmployeeDetails;
 import de.yessoft.android.entity.EmployeeInfo;
 import de.yessoft.android.entity.Project;
+import de.yessoft.android.fragments.EmployeeListFragment.childs.EmployeeBlogsFragment;
 import de.yessoft.android.fragments.EmployeeListFragment.childs.EmployeePersonalInfoFragment;
+import de.yessoft.android.fragments.EmployeeListFragment.childs.EmployeeProjectsFragment;
+import de.yessoft.android.fragments.EmployeeListFragment.childs.EmployeeSkillsFragment;
 
 public class EmployeeListFragment extends Fragment implements IEmployeeList {
 
@@ -43,8 +47,8 @@ public class EmployeeListFragment extends Fragment implements IEmployeeList {
     @BindView(R.id.nv_employee_sections)
     BottomNavigationView nvEmployeeSections;
 
-    @BindView(R.id.tv_position_name)
-    TextSwitcher tvPositionName;
+    @BindView(R.id.ts_position_name)
+    TextView tvPositionName;
 
     // endregion
 
@@ -80,14 +84,24 @@ public class EmployeeListFragment extends Fragment implements IEmployeeList {
         // TODO Replace With DB Query
         mEmployeeList = new ArrayList<>();
 
-        EmployeeInfo info = new EmployeeInfo("Mohammad", "Developer", "24", "Work Hard and Aim High", "http://www/google.com");
-        EmployeeInfo info2 = new EmployeeInfo("Mohammad", "Designer", "24", "Work Hard and Aim High", "http://www/google.com");
+        EmployeeInfo info = new EmployeeInfo("Mohammad", "Developer", "24", "Work Hard and Aim High", "https://picsum.photos/200/300");
+        EmployeeInfo info2 = new EmployeeInfo("Mohammad", "Designer", "24", "Work Hard and Aim High", "https://picsum.photos/300/300");
+        List<Project> projectList = new ArrayList<>();
+        projectList.add(new Project("ishtar art", "http://google.com","Jun 19", "Aug 19"));
+
+        List<String> skillList = new ArrayList<>();
+        skillList.add("Communication");
+        skillList.add("Design Patterns");
 
         EmployeeDetails employeeDetails = new EmployeeDetails();
         employeeDetails.setInfo(info);
+        employeeDetails.setProjects(projectList);
+        employeeDetails.setSkills(skillList);
 
         EmployeeDetails employeeDetails2 = new EmployeeDetails();
-        employeeDetails.setInfo(info2);
+        employeeDetails2.setInfo(info2);
+        employeeDetails2.setProjects(projectList);
+        employeeDetails2.setSkills(skillList);
 
         // As Placeholders
         for (int i = 0; i < 10; i++)
@@ -110,6 +124,10 @@ public class EmployeeListFragment extends Fragment implements IEmployeeList {
         });
 
         employeesLayoutManager = (CardSliderLayoutManager) rvEmployeesCards.getLayoutManager();
+
+        int[] animH = new int[]{R.anim.slide_in_right, R.anim.slide_out_left};
+        int[] animV = new int[]{R.anim.slide_in_top, R.anim.slide_out_bottom};
+        updateActiveFragment(animH, animV);
     }
 
     // region View Updates
@@ -128,40 +146,40 @@ public class EmployeeListFragment extends Fragment implements IEmployeeList {
 
     private void displayProjects() {
         getChildFragmentManager().beginTransaction()
-                .replace(R.id.fl_employees, new EmployeePersonalInfoFragment())
+                .replace(R.id.fl_employees, new EmployeeProjectsFragment())
                 .commit();
     }
 
     private void displayProjects(int[] animH, int[] animV) {
         getChildFragmentManager().beginTransaction()
                 .setCustomAnimations(animV[0], animV[1])
-                .replace(R.id.fl_employees, new EmployeePersonalInfoFragment())
+                .replace(R.id.fl_employees, new EmployeeProjectsFragment())
                 .commit();
     }
 
     private void displaySkills() {
         getChildFragmentManager().beginTransaction()
-                .replace(R.id.fl_employees, new EmployeePersonalInfoFragment())
+                .replace(R.id.fl_employees, new EmployeeSkillsFragment())
                 .commit();
     }
 
     private void displaySkills(int[] animH, int[] animV) {
         getChildFragmentManager().beginTransaction()
                 .setCustomAnimations(animV[0], animV[1])
-                .replace(R.id.fl_employees, new EmployeePersonalInfoFragment())
+                .replace(R.id.fl_employees, new EmployeeSkillsFragment())
                 .commit();
     }
 
     private void displayBlogs() {
         getChildFragmentManager().beginTransaction()
-                .replace(R.id.fl_employees, new EmployeePersonalInfoFragment())
+                .replace(R.id.fl_employees, new EmployeeBlogsFragment())
                 .commit();
     }
 
     private void displayBlogs(int[] animH, int[] animV) {
         getChildFragmentManager().beginTransaction()
                 .setCustomAnimations(animV[0], animV[1])
-                .replace(R.id.fl_employees, new EmployeePersonalInfoFragment())
+                .replace(R.id.fl_employees, new EmployeeBlogsFragment())
                 .commit();
     }
 
@@ -191,9 +209,8 @@ public class EmployeeListFragment extends Fragment implements IEmployeeList {
             animV[1] = R.anim.slide_out_top;
         }
 
-        tvPositionName.setInAnimation(getContext(), animH[0]);
-        tvPositionName.setOutAnimation(getContext(), animH[1]);
-        tvPositionName.setText(mEmployeeList.get(pos).getInfo().getPosition());
+        if (mEmployeeList.get(pos).getInfo().getPosition() != null)
+            tvPositionName.setText(mEmployeeList.get(pos).getInfo().getPosition());
 
         updateActiveFragment(animH, animV);
 
--- app/src/main/java/de/yessoft/android/fragments/EmployeeListFragment/childs/EmployeeSkillsFragment.java
+++ app/src/main/java/de/yessoft/android/fragments/EmployeeListFragment/childs/EmployeeSkillsFragment.java
@@ -2,6 +2,7 @@ package de.yessoft.android.fragments.EmployeeListFragment.childs;
 
 import android.content.Context;
 import android.os.Bundle;
+import android.util.TypedValue;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -10,15 +11,44 @@ import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import com.google.android.material.chip.Chip;
+import com.google.android.material.chip.ChipDrawable;
+import com.google.android.material.chip.ChipGroup;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 import de.yessoft.android.R;
 import de.yessoft.android.fragments.EmployeeListFragment.IEmployeeList;
 
 public class EmployeeSkillsFragment extends Fragment {
-    IEmployeeList mParent;
+    private IEmployeeList mParent;
+
+    @BindView(R.id.skills_chip_group)
+    ChipGroup skillsChips;
 
     @Override
     public void onAttach(@NonNull Context context) {
         super.onAttach(context);
         mParent = (IEmployeeList) getParentFragment();
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_employee_skills, container, false);
+        ButterKnife.bind(this, view);
+
+        initChips();
+
+        return view;
+    }
+
+    private void initChips() {
+        for (String skill : mParent.getSkillList()) {
+            assert getContext() != null;
+            Chip chip = new Chip(getContext());
+            chip.setText(skill);
+            skillsChips.addView(chip);
+        }
     }
 }
